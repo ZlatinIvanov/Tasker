c2b51328b063c22325dacb5e951f56f0de1af9f4
ZlatinIvanov <zlatin89@gmail.com>
2024-03-30 13:04:43 +0200

    Added more adjustments to the Task model and the templates.

--- tasker/tasks/urls.py
+++ tasker/tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from tasker.tasks.views import TaskCreateView, task_list, TaskDetailsView
+from tasker.tasks.views import TaskCreateView, task_list, TaskDetailsView, TaskListView
 
 urlpatterns = (
     path('create_task/', TaskCreateView.as_view(), name='create_task'),
     path('tasks_list/', task_list, name='tasks_list'),
+    path('tasks/', TaskListView.as_view(), name='tasks_list'),
     path(
         "task/<int:pk>/", include([
             path("", TaskDetailsView.as_view(), name="task_details"),
--- tasker/tasks/views.py
+++ tasker/tasks/views.py
@@ -21,6 +21,7 @@ class TaskCreateView(auth_mixin.LoginRequiredMixin, views.CreateView):
     def get_success_url(self):
         return reverse('tasks_list')
 
+
 class TaskDetailsView(auth_mixin.LoginRequiredMixin, views.DetailView):
     queryset = Tasks.objects.all()
     template_name = "tasks/task_details.html"
@@ -32,3 +33,21 @@ class TaskDetailsView(auth_mixin.LoginRequiredMixin, views.DetailView):
         filtered_queryset = queryset.filter(pk=pk)
         return filtered_queryset
 
+
+class TaskListView(views.ListView):
+    model = Tasks
+    template_name = 'tasks/tasks_list.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        sort_by = self.request.GET.get('sort')
+        if sort_by == 'priority':
+            return Tasks.objects.order_by('priority')
+        elif sort_by == 'created_at':
+            return Tasks.objects.order_by('created_at')
+        elif sort_by == 'due_date':
+            return Tasks.objects.order_by('due_date')
+        elif sort_by == 'title':
+            return Tasks.objects.order_by('title')
+        else:
+            return Tasks.objects.all()
